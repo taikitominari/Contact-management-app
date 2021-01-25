156447a1c3381ed4df775a4f0dc5cb65ea0126b9
taikitominari <taikitominari.co.jp@icloud.com>
2021-01-25 23:57:32 +0900

    vue.lsをCDNで読み込み vue.jsで文字の出力

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello vue.js!!!',
+    count: 0,
+    user: {
+      lastName: 'Yamada',
+      firistName: 'Taro',
+      prefecture: 'Tokyo'
+    },
+    colors: ['Red', 'Green', 'Blue']
+  }
+})
