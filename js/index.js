4c811e93acf3f880af140a40a56510eb7849f43d
taikitominari <taikitominari.co.jp@icloud.com>
2021-01-30 00:33:27 +0900

    list追加時の(完了,削除)機能追加

--- js/index.js
+++ js/index.js
@@ -9,11 +9,15 @@ let app = new Vue({
       if(this.newItem == "") return;
 
       let todo = {
-        item: this.newItem
+        item: this.newItem,
+        isDone: false
       };
 
       this.todos.push(todo)
       this.newItem = "";
+    },
+    deleteBtn: function(index) {
+      this.todos.splice(index, 1)
     }
   }
 })
