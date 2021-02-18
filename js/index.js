36286adfbdc898d1a198b9467eea18fcc2da109d
taikitominari <taikitominari.co.jp@icloud.com>
2021-02-18 22:32:28 +0900

    watchオプションの実装 $watchメソッドの実装

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
-let app = new Vue({
-	el: '#app',
-  data: {
-    message: 'Hello Vue.js'
+const vm = new Vue ({
+  el: '#app',
+  data() {
+    return {
+      numbers: [],
+      total_num: 0
+    }
+  },
+  // watchオプション
+  watch: {
+    number(value){
+      this.total_num = 0
+      this.number.forEach(number => {
+        this.total_num += number
+      })
+    }
   }
+})
+
+window.vm = vm
+
+// $メソッド
+vm.$watch(function() {
+  return this.message 
+}, function (message) {
+  console.log('変更後の値:' + message)
 })
