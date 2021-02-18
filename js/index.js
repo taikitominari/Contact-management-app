e0e692a0864702cc72ec98950f3199e06401cef1
taikitominari <taikitominari.co.jp@icloud.com>
2021-02-18 21:35:10 +0900

    v-preディレクティブの実装 v-htmlディレクティブの実装

--- js/index.js
+++ js/index.js
@@ -1,11 +1,6 @@
 let app = new Vue({
 	el: '#app',
   data: {
-  	message: 'Hello Vue.js!'
-  },
-  methods: {
-    clickHandler: function(event) {
-      this.message = this.message.split('').reverse().join('')
-    }
+    message: 'Hello <span style="color:red;">Vue.js!</span>'
   }
 })
