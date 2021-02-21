f1254be57c902a29c49b85e7f7700e07688d9d6d
taikitominari <taikitominari.co.jp@icloud.com>
2021-02-21 11:38:08 +0900

    スロット機能の練習

--- js/index.js
+++ js/index.js
@@ -1,43 +1,15 @@
-const UserForm = {
-  template:`
-    <div>
-      <div>名前入力フォーム</div>
-      <input v-model='user_name'>
-      <button @click='update'>名前変更</button>
-    </div>
-  `,
-  props: {
-    userName: { type: String, required: true }
-  },
-  data() {
-    return {
-      user_name: this.userName
-    }
-  },
-  methods: {
-    update() {
-      this.$emit('update:user-name', this.user_name)
-    }
-  }
-}
-
-const UserDetail = {
-  components: {
-    'user-form': UserForm
-  },
-  data() {
-    return {
-      user_name: '山田 太郎'
-    }
-  },
+const Layout = {
   template: `
-    <div>
-      <div>
-        <span>ユーザー名: {{ user_name }}</span>
-      </div>
-      <div>
-        <user-form :user-name='user_name' @update:user-name='user_name = $event'></user-form>
-      </div>
+    <div class="container">
+      <header>
+        <slot name="header"></slot>
+      </header>
+      <main>
+        <slot></slot>
+      </main>
+      <footer>
+        <slot name="footer"></slot>
+      </footer>
     </div>
   `
 }
@@ -45,10 +17,6 @@ const UserDetail = {
 const vm = new Vue({
   el: '#app',
   components: {
-    'user-detail': UserDetail
+    'layout': Layout
   }
 })
-
-{/* <div>
-<userform></userform>
-</div> */}
