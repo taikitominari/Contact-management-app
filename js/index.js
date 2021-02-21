238fb22918034499d74d53e14212884874950a64
taikitominari <taikitominari.co.jp@icloud.com>
2021-02-21 10:10:00 +0900

    コンポーネントの作成(グローバル登録)

--- js/index.js
+++ js/index.js
@@ -1,10 +1,32 @@
-const vm = new Vue ({
-  el: '#app',
+Vue.component( 'list-title' , {
+  template: `
+    <h2>ユーザーリスト</h2>
+  `
+})
+Vue.component('user-list', {
   data() {
     return {
-      message: null
+      users: [
+        {id: 1, name: 'ユーザー１'},
+        {id: 2, name: 'ユーザー2'},
+        {id: 3, name: 'ユーザー3'},
+        {id: 4, name: 'ユーザー4'},
+        {id: 5, name: 'ユーザー5'}
+      ]
     }
-  }
- })
+  },
+  template:`
+  <div>
+    <list-title></list-title>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }}
+      </li>
+    </ul>
+  </div>
+  `
+})
 
-window.vm = vm
+const vm = new Vue({
+  el: '#app',
+})
