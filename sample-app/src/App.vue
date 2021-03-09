824848ab6aacd29e09b4d014737eedf2a4cdc6c7
taikitominari <taikitominari.co.jp@icloud.com>
2021-03-09 22:54:39 +0900

    v-app-bar-nav-iconでのMenuの開閉を実装

--- sample-app/src/App.vue
+++ sample-app/src/App.vue
@@ -1,32 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+
+    <v-app-bar-nav-icon @click="openSideMenu"></v-app-bar-nav-icon>
+    <v-toolbar-title>マイアドレス帳</v-toolbar-title>
+    <v-spacer></v-spacer>
+
+    </v-app-bar>
+    <v-content>
+      <SideNav/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import SideNav from './components/SideNav'
+export default {
+  name: 'App',
+  components: {
+    SideNav
+  },
+  data: () => ({
+    //
+  }),
+  methods: {
+    openSideMenu () {
+      this.$store.dispatch('toggleSideMenu')
     }
   }
-}
-</style>
+};
+</script>
