1bdd0fc1237671e6366a10fe4d5ebabd869bd349
taikitominari <taikitominari.co.jp@icloud.com>
2021-04-18 11:23:51 +0900

    firebaseのscriptを(index.html)⇨main.jsに移動

--- sample-app/src/main.js
+++ sample-app/src/main.js
@@ -2,9 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJrJwq27bI52NwgTROEaeGpitgjVgEiMc",
+  authDomain: "my-address-pj-3a951.firebaseapp.com",
+  projectId: "my-address-pj-3a951",
+  storageBucket: "my-address-pj-3a951.appspot.com",
+  messagingSenderId: "871133339346",
+  appId: "1:871133339346:web:be292f9898744e4afad70b",
+  measurementId: "G-N0KVYDX4K2"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
 
 new Vue({
   router,
