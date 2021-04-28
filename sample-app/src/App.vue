ef0634ac48c9a35a9b620a5dec33af0a4979c65c
taikitominari <taikitominari.co.jp@icloud.com>
2021-04-28 21:38:56 +0900

    ログイン,ログアウト時のページのルート指定

--- sample-app/src/App.vue
+++ sample-app/src/App.vue
@@ -35,8 +35,10 @@ export default {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.setLoginUser(user)
+        if (this.$router.currentRoute.name === 'home') this.$router.push({ name: 'addresses'})
       } else{
         this.deleteLoginUser()
+        this.$router.push({ name: 'home' })
       }
     })
   },
